d642e8ad69efb4ff7793f674e739839599744869
Geert Hoekstra <geert_hoekstra@nl.ibm.com>
2025-03-27 13:25:25 +0100

    Adapted DB2ZConfigurationExtension to implement getNamespace()

--- flyway-community-db-support/src/main/java/org/flywaydb/community/database/db2z/DB2ZConfigurationExtension.java
+++ flyway-community-db-support/src/main/java/org/flywaydb/community/database/db2z/DB2ZConfigurationExtension.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) Red Gate Software Ltd 2010-2022
+ * Copyright (C) Red Gate Software Ltd 2010-2023
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,6 +33,11 @@ public class DB2ZConfigurationExtension implements ConfigurationExtension {
    */
   private String sqlId = "";
 
+
+  @Override
+  public String getNamespace() {
+    return "db2z";
+  }
 
   @Override
   public void extractParametersFromConfiguration(Map<String, String> configuration) {
