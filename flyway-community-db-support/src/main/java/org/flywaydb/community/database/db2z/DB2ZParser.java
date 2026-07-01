a282623cb9c6a2828430537bfe51589cc87f7874
Geert Hoekstra <geert_hoekstra@nl.ibm.com>
2026-07-01 10:35:15 +0200

    Fixed issue with CALL statements preceded by commended CALL statement(s)

--- flyway-community-db-support/src/main/java/org/flywaydb/community/database/db2z/DB2ZParser.java
+++ flyway-community-db-support/src/main/java/org/flywaydb/community/database/db2z/DB2ZParser.java
@@ -22,7 +22,6 @@ import org.flywaydb.core.internal.sqlscript.Delimiter;
 import org.flywaydb.core.internal.sqlscript.ParsedSqlStatement;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -49,7 +48,7 @@ public class DB2ZParser extends Parser {
     private static final StatementType DB2Z_CALL_STATEMENT = new StatementType();
     // Do not assume first line is beginning of CALL statement. Maybe comment or whitelines first...
     private static final Pattern DB2Z_CALL_WITH_PARMS_REGEX = Pattern.compile(
-            "CALL\\s+(?<procname>([^\\s]+\\.)?[^\\s]+)(\\(\\s*(?<args>\\S.*)\\s*\\))", Pattern.CASE_INSENSITIVE);
+            "^CALL\\s+(?<procname>([^\\s]+\\.)?[^\\s]+)(\\(\\s*(?<args>\\S.*)\\s*\\))$", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
 
     //Split on comma if that comma has zero, or an even number of quotes ahead
     private static final Pattern PARMS_SPLIT_REGEX = Pattern.compile(",(?=(?:[^']*'[^']*')*[^']*$)");
@@ -73,8 +72,9 @@ public class DB2ZParser extends Parser {
                                                  StatementType statementType, boolean canExecuteInTransaction,
                                                  Delimiter delimiter, String sql
     ) throws IOException {
-        LOG.debug(sql);
+        LOG.debug("createStatement: sql=" + sql);
         if (statementType == DB2Z_CALL_STATEMENT) {
+            LOG.debug("createStatement: Processing DB2Z CALL statement");
             Matcher callMatcher = DB2Z_CALL_WITH_PARMS_REGEX.matcher(sql);
             if(callMatcher.find()) {
                 String procName = callMatcher.group("procname");
@@ -99,8 +99,11 @@ public class DB2ZParser extends Parser {
                 return new DB2ZCallProcedureParsedStatement(statementPos, statementLine, statementCol,
                     sql, delimiter, canExecuteInTransaction, procName, parms);
             }
+            LOG.debug("createStatement: DB2Z CALL statement did not have parameters");
+
+        } else {
+            LOG.debug("createStatement: Not a DB2Z CALL statement" );
         }
-        LOG.debug("createStatement: DB2Z CALL no parms " + statementType + " " + sql);
         return super.createStatement(reader, recorder, statementPos, statementLine, statementCol,
                 nonCommentPartPos, nonCommentPartLine, nonCommentPartCol,
                 statementType, canExecuteInTransaction, delimiter, sql
